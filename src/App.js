9f085a22749b19d204fde471a6ba3550289acc03
mbalabanov <marincomics@gmail.com>
2022-09-30 17:28:59 +0300

    Adds initial API call to get artworks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import AdviceSection from "./sections/Advice"
-import ArtsSection from "./sections/Art"
-import UsersSection from "./sections/Users"
-import "./styles.css"
+import AdviceSection from "./sections/Advice";
+import ArtsSection from "./sections/Art";
+import UsersSection from "./sections/Users";
+
+import { useState, useEffect } from "react";
+
+import "./styles.css";
 
 function App() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.artic.edu/api/v1/artworks?page=2&limit=100")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.data);
+        setArtworks(res.data);
+      });
+  }, []);
+
   return (
     <div className="main-layout">
-      <ArtsSection />
+      <ArtsSection artworks={artworks} />
       <UsersSection />
       <AdviceSection />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-function ArtsSection() {
+function ArtsSection(props) {
+  console.log(props);
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        {props.artworks.map((artwork, i) => (
+          <p key={i}>{artwork.title}</p>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
